6581f989f014255f79fdd4b75bed4b7b16460b03
Joshgun Rzabayli <joshgun.rzabayli@gmail.com>
2024-04-02 20:45:49 +0400

    API url was configured for deployment.

--- task_management_backend/task_management_backend/settings.py
+++ task_management_backend/task_management_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ga!*zrpg99y6sns8icb8i9_gue#n@6e#qhxw4p9%*#-h&)sdbd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
--- task_management_backend/task_management_backend/urls.py
+++ task_management_backend/task_management_backend/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls')),
+    path('', include('tasks.urls')),
     path('authentication/', include('authentication.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
